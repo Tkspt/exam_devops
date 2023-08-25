6e3f253fc49224c7aae5230f4a628a29f5939c6e
Mor Codou SECK <papemor1994@gmail.com>
2023-08-25 19:59:37 +0000

    create seconds level folders close #2

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ path = os.getcwd()
 directory_manager = DirectoryManager(path)
 git_manager = GitManager(path)
 
-first_level_folders = ["data", "docs",
-                       "models", "notebooks", "reports", "src"]
+second_level_folders = ["data/cleaned", "data/raw",]
 
-directory_manager.createEmptyDirectories(first_level_folders)
+directory_manager.createEmptyDirectories(second_level_folders)
 
 git_manager.add_into_local_repos()
-git_manager.add_commit('create first level folders close #1')
+git_manager.add_commit('create seconds level folders close #2')
 git_manager.push_to_remote()
